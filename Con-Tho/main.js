83511f9ae6019eb3a79043ee1e502cf916557ca6
Code là Ghiền <53667931+codelaghien@users.noreply.github.com>
2020-03-29 14:49:21 -0700

    Add files via upload

--- Con-Tho/main.js
+++ Con-Tho/main.js
@@ -1,27 +1,31 @@
-let dansach = [{age: 0, birth: 0, death: false}];
+let dansach = [{ age: 0, birth: 0, death: false }];
 let tongSoTho = 1;
 let tongSoThoBan = 0;
 let thang = 0;
 let baocao;
 
-function TangThang() { 
+function TangThang() {
     if (!baocao) {
         baocao = document.getElementById('baocao');
     }
     thang++;
+    let themTho = 0;
     dansach.forEach(contho => {
         contho.age++;
         if (contho.age > 1) {
             tongSoTho++;
-            dansach.push({age: 0, birth: 0, death: false});
+            themTho++;
             contho.birth++;
             if (contho.birth === 10) {
                 contho.death = true;
                 tongSoThoBan++;
             }
         }
-    })
+    });
     dansach = dansach.filter(contho => !contho.death);
+    for (let i = 0; i < themTho; i++) {
+        dansach.push({ age: 0, birth: 0, death: false });
+    }
     console.log('Tháng thứ ' + thang);
     console.log('Tổng số thỏ = ' + tongSoTho);
     console.log('Tổng số thỏ bán = ' + tongSoThoBan);
