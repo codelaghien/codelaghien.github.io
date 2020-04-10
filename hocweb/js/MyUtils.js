592caf14177b14d2c50d9f55bb5fba45920f7b69
codelaghien <codelaghien.github.io>
2020-04-10 09:06:35 -0700

    test

--- hocweb/js/MyUtils.js
+++ hocweb/js/MyUtils.js
@@ -13,7 +13,7 @@ function submitLogin() {
 	// console.log('email = ' + sessionStorage.getItem('email'));
 	// window.location.replace('index.html');
 
-	$.get('http://codelaghien-001-site2.ftempurl.com/codelaghien/login.asp', {
+	$.get('https://codelaghien.club/web/login.asp', {
 		username: email,
 		password: password,
 	})
