98dfbdc4d4ededd886e9f051a8bad63f681c50bd
codelaghien <codelaghien.github.io>
2020-04-10 10:50:02 -0700

    kiem tra login moi trang

---
+++ hocweb/js/Auth.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+	const email = sessionStorage.getItem('email');
+	console.log('email', email);
+	if (!email || email === '') {
+		window.location.replace('login.html');
+	}
+};
